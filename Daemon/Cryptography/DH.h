7cbffa14062658e4ef823f439890f89abdc326fe
alleksh <allekshey@gmail.com>
2019-06-18 15:43:24 +0300

    Move
    Moved from SocketService/Cryptography.h to Cryptography/DH

---
+++ Daemon/Cryptography/DH.h
@@ -0,0 +1,35 @@
+#include <gmpxx.h>
+#include <string>
+namespace Cryptography {
+    namespace DH {
+        mpz_t G, P;
+        void load(::std::string g, ::std::string p) {
+            mpz_set_str(G, g.c_str(), 10);
+            mpz_set_str(P, p.c_str(), 10);
+        } 
+        
+        struct Connection {
+            mpz_t a;
+            mpz_t A;
+            mpz_t K;
+            Connection(::std::string g, ::std::string p){
+                load(g, p);
+                Connection();
+            }
+            Connection() {
+                mpz_init(a); mpz_init(A); mpz_init(K);
+                CSPNG r;
+                ::std::string v = r.getStr() + r.getStr() + r.getStr() + r.getStr();
+                mpz_set_str(a, v.c_str(), 0x10);
+                mpz_powm(A,G,a,P);
+            }
+            void ReloadSecretKey(::std::string B) {
+                mpz_set_str(K, B.c_str(), 66);
+                mpz_powm(K,K,a,P);
+            }
+            ~Connection() {
+                mpz_clear(a); mpz_clear(A); mpz_clear(K);
+            }
+        };
+    }
+}
