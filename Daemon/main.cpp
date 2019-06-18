560f3a6606dad118481318a1a72cdb2862fe370e
alleksh <allekshey@gmail.com>
2019-06-18 15:43:10 +0300

    Update main.cpp

--- Daemon/main.cpp
+++ Daemon/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-
-
-#include "SocketService/SocketService.h"
 #include <fstream>
+#include "SocketService/SocketService.h"
 #include "json.hpp"
 #include <string>
 int main() {
-    ::std::cout << "Starting...\n";
+    ::std::cout << "Starting\n";
     ::std::ifstream configFile("config.json");
     ::std::string value="", buf = "";
     while(getline(configFile, buf)) {
@@ -14,26 +12,38 @@ int main() {
     }
     configFile.close();
     auto config = nlohmann::json::parse(value);
-    SocketService ss(
-        config["DBName"].get<::std::string>(),
-        config["safeMoney"].get<bool>(),
-        config["safeMoneyDelay"].get<uint32_t>(),
-        config["port"].get<uint32_t>(),
-        config["shardedEconomy"].get<bool>(),
-        config["shardedServers"].get<bool>(),
-        config["RSA_ENCRYPTION_FIRST_PRIME"].get<::std::string>(),
-        config["RSA_ENCRYPTION_SECOND_PRIME"].get<::std::string>());
-    std::string encrypted;
-    ::std::string input;
-    ::std::cout << "Enter your message: ";
-    ::std::cin >> input;
-    encrypted = ss.rsa->Encrypt(input.c_str(), input.size());
+//    SocketService ss(
+//        config["DBName"].get<::std::string>(),
+ //       config["safeMoney"].get<bool>(),
+  //      config["safeMoneyDelay"].get<uint32_t>(),
+   //     config["port"].get<uint32_t>(),
+    //    config["shardedEconomy"].get<bool>(),
+     //   config["shardedServers"].get<bool>(),
+      //  config["RSA_ENCRYPTION_FIRST_PRIME"].get<::std::string>(),
+       // config["RSA_ENCRYPTION_SECOND_PRIME"].get<::std::string>());
+using namespace Cryptography;
+using namespace AES;
+    char* out;
+    char* out2;
+    ::std::string in = "abcdef";
+    ::std::string key = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    for(int i=0;i<in.size();i++) {
+        std::cout << int16_t(in.c_str()[i])  << " ";
+    }
+    std::cout << std::endl;
+    auto len = Encrypt(in.c_str(),6,&out,key.c_str());
+    for(int i=0;i<len;i++) {
+        std::cout << int16_t(out[i])  << " ";
+    }
+    std::cout << std::endl;
+    Decrypt(out, len, &out2, key.c_str());
+    for(int i=0;i<len;i++) {
+        std::cout << int16_t(out2[i])  << " ";
+    }
+    free(out);
+    free(out2);
+}
 
-    char* decrypted;
-    ::std::cout << "Encrypted message: ";
-    std::cout <<encrypted << ::std::endl;
 
-    ss.rsa->Decrypt(decrypted, encrypted);
-    
-    ::std::cout << "Decrypted message: " << ::std::string(decrypted) << std::endl;
-} 
+
+
