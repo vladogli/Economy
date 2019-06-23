8e541e5019d90f26b7bea9c5144fbc40bf8b8db9
alleksh <allekshey@gmail.com>
2019-06-23 22:47:32 +0300

    Update main.cpp

--- Daemon/main.cpp
+++ Daemon/main.cpp
@@ -3,46 +3,53 @@
 #include "SocketService/SocketService.h"
 #include "json.hpp"
 #include <string>
+#include <chrono>
+#define TEST
+#ifndef TEST
+
+#define MAIN_CLUSTER
 int main() {
     ::std::cout << "Starting\n";
-    ::std::ifstream configFile("config.json");
+#ifdef MAIN_CLUSTER
+    ::std::ifstream configFile("MainCluster.json");
+#else
+    ::std::ifstream configFile("Cluster.json");
+#endif
     ::std::string value="", buf = "";
     while(getline(configFile, buf)) {
         value +=buf + "\n";
     }
     configFile.close();
     auto config = nlohmann::json::parse(value);
-//    SocketService ss(
-//        config["DBName"].get<::std::string>(),
- //       config["safeMoney"].get<bool>(),
-  //      config["safeMoneyDelay"].get<uint32_t>(),
-   //     config["port"].get<uint32_t>(),
-    //    config["shardedEconomy"].get<bool>(),
-     //   config["shardedServers"].get<bool>(),
-      //  config["RSA_ENCRYPTION_FIRST_PRIME"].get<::std::string>(),
-       // config["RSA_ENCRYPTION_SECOND_PRIME"].get<::std::string>());
-using namespace Cryptography;
-using namespace AES;
-    char* out;
-    char* out2;
-    ::std::string in = "abcdef";
-    ::std::string key = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    for(int i=0;i<in.size();i++) {
-        std::cout << int16_t(in.c_str()[i])  << " ";
-    }
-    std::cout << std::endl;
-    auto len = Encrypt(in.c_str(),6,&out,key.c_str());
-    for(int i=0;i<len;i++) {
-        std::cout << int16_t(out[i])  << " ";
-    }
-    std::cout << std::endl;
-    Decrypt(out, len, &out2, key.c_str());
-    for(int i=0;i<len;i++) {
-        std::cout << int16_t(out2[i])  << " ";
-    }
-    free(out);
-    free(out2);
+#ifdef MAIN_CLUSTER
+    SocketService ss(
+        config["DBName"].get<::std::string>(),
+        config["safeMoney"].get<bool>(),
+        config["safeMoneyDelay"].get<uint32_t>(),
+        config["port"].get<uint32_t>(),
+        config["shardedEconomy"].get<bool>(),
+        config["shardedServers"].get<bool>(),
+        config["RSA_ENCRYPTION_FIRST_PRIME"].get<::std::string>(),
+        config["RSA_ENCRYPTION_SECOND_PRIME"].get<::std::string>()
+        config["DSA_ENCRYPTION_3072_PRIME"].get<::std::string>(),
+        config["DSA_ENCRYPTION_256_PRIME"].get<::std::string>());
+#else
+    SocketService ss(
+        config["DBName"].get<::std::string>(),
+        config["safeMoney"].get<bool>(),
+        config["safeMoneyDelay"].get<uint32_t>(),
+        config["port"].get<uint32_t>(),
+        config["shardedEconomy"].get<bool>(),
+        config["DSA_SUB_KEY"].get<::std::string>(),
+        config["RSA_PUBLIC_CONSTANT"].get<::std::string>());
+#endif
 }
+#else
+#include "test.h"
+int main() {
+    TestCryptography();
+}
+#endif
 
 
 
